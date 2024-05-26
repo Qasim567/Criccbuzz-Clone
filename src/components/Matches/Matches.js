037e7b54f425cda4c86c549befade077c4bb2c7d
Qasim Farooq <qasimfarooq631@gmail.com>
2024-05-26 17:26:44 +0500

    env

--- src/components/Matches/Matches.js
+++ src/components/Matches/Matches.js
@@ -2,19 +2,22 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './Matches.css';
 import { Link } from 'react-router-dom';
+import API_KEY from '../../config';
 
 export default function Matches() {
+  const apikey = process.env.REACT_APP_API_KEY;
   const [matches, setMatches] = useState([]);
   const [matchState, setMatchState] = useState('live');
+  const [matchType, setMatchType] = useState('international');
   const [loading, setLoading] = useState(true);
-  
-  const fetchData = async (selectedMatchState) => {
+
+  const fetchData = async (selectedMatchState, selectedMatchType) => {
     const options = {
       method: 'GET',
       url: 'https://unofficial-cricbuzz.p.rapidapi.com/matches/list',
       params: { matchState: selectedMatchState },
       headers: {
-        'X-RapidAPI-Key': '9d5ecbf72fmsh80034365670a1d2p1317c3jsna96cf5494285',
+        'X-RapidAPI-Key': API_KEY,
         'X-RapidAPI-Host': 'unofficial-cricbuzz.p.rapidapi.com'
       }
     };
@@ -22,7 +25,10 @@ export default function Matches() {
     try {
       setLoading(true);
       const response = await axios.request(options);
-      setMatches(response.data.typeMatches);
+      const filteredMatches = response.data.typeMatches.filter(
+        match => match.matchType.toLowerCase() === selectedMatchType
+      );
+      setMatches(filteredMatches);
     } catch (error) {
       console.error(error);
     } finally {
@@ -31,16 +37,8 @@ export default function Matches() {
   };
 
   useEffect(() => {
-    fetchData(matchState);
-  }, [matchState]);
-
-  const chunkArray = (array, size) => {
-    const result = [];
-    for (let i = 0; i < array.length; i += size) {
-      result.push(array.slice(i, i + size));
-    }
-    return result;
-  };
+    fetchData(matchState, matchType);
+  }, [matchState, matchType]);
 
   const hasNoLiveMatches = () => {
     return matchState === 'live' && (!matches || matches.length === 0);
@@ -69,22 +67,52 @@ export default function Matches() {
           Upcoming
         </button>
       </div>
+      <div className="format-buttons my-2">
+        <button
+          className={matchType === 'international' ? 'active' : ''}
+          onClick={() => setMatchType('international')}
+        >
+          International
+        </button>
+        <button
+          className={matchType === 'league' ? 'active' : ''}
+          onClick={() => setMatchType('league')}
+        >
+          Leagues
+        </button>
+        <button
+          className={matchType === 'domestic' ? 'active' : ''}
+          onClick={() => setMatchType('domestic')}
+        >
+          Domestic
+        </button>
+        <button
+          className={matchType === 'women' ? 'active' : ''}
+          onClick={() => setMatchType('women')}
+        >
+          Women
+        </button>
+      </div>
       {hasNoLiveMatches() && <p className='my-4'>There are no live matches at the moment. Please check back later.</p>}
-      {!loading && matches && matches.map((matchType, matchTypeIndex) => (
+      {!loading && matches && matches.map((matchTypeData, matchTypeIndex) => (
         <div className="my-4" key={matchTypeIndex}>
-          {matchType.seriesAdWrapper && matchType.seriesAdWrapper.map((seriesWrapper, seriesWrapperIndex) => (
+          {matchTypeData.seriesAdWrapper && matchTypeData.seriesAdWrapper.map((seriesWrapper, seriesWrapperIndex) => (
             seriesWrapper.seriesMatches && (
               <div key={seriesWrapperIndex}>
-                {chunkArray(seriesWrapper.seriesMatches.matches, 3).map((matchRow, rowIndex) => (
+                {seriesWrapper.seriesMatches.matches.reduce((rows, match, index) => {
+                  if (index % 3 === 0) rows.push([]);
+                  rows[rows.length - 1].push(match);
+                  return rows;
+                }, []).map((matchRow, rowIndex) => (
                   <div className="row" key={rowIndex}>
                     {matchRow.map((match, matchIndex) => (
                       <div key={matchIndex} className="col-md-4 mb-4">
-                        <div className="card mb-4 rounded-3 shadow-sm">
+                        <div className="card shadow-sm">
                           <div className="card-header py-3 bg-success">
-                            <h4 className="my-0 fw-normal primary"><b>{matchType.matchType}</b></h4>
+                            <h4><b>{matchTypeData.matchType}</b></h4>
                           </div>
                           <div className="card-body">
-                            <p className="card-title pricing-card-title">
+                            <p>
                               <b>{match.matchInfo.seriesName}, {match.matchInfo.matchDesc}</b><br />
                               <p>
                                 <b>Venue:</b> {match.matchInfo.venueInfo.city}, {match.matchInfo.venueInfo.ground}
@@ -105,13 +133,11 @@ export default function Matches() {
                               </li>
                               <li style={{ color: 'red' }}>{match.matchInfo.status}</li>
                             </ul>
-                            {/* <Link className='link' to={`/commentary/${match.matchInfo.matchId}`}>Full Commentary</Link> */}
                             <Link className='link' to={`/scoreboard/${match.matchInfo.matchId}`}>Scoreboard</Link>
                             <Link className='link mx-2' to={`/matchinfo/${match.matchInfo.matchId}`}>Match Facts</Link>
                           </div>
                         </div>
                       </div>
-
                     ))}
                   </div>
                 ))}
