a56ba780a81f00ef0073c394ec111736fefd0696
Qasim Farooq <qasimfarooq631@gmail.com>
2024-05-28 12:57:46 +0500

    commentry

--- src/components/Matches/Commentry.js
+++ src/components/Matches/Commentry.js
@@ -1,59 +1,59 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
 
 const Commentary = () => {
+  const apikey = process.env.REACT_APP_API_KEY_1;
   const { matchId } = useParams();
   const [commentaryData, setCommentaryData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    const fetchCommentaryData = async () => {
-      const options = {
-        method: 'GET',
-        url: 'https://unofficial-cricbuzz.p.rapidapi.com/matches/get-commentaries',
-        params: { matchId },
-        headers: {
-          'X-RapidAPI-Key': '9d5ecbf72fmsh80034365670a1d2p1317c3jsna96cf5494285',
-          'X-RapidAPI-Host': 'unofficial-cricbuzz.p.rapidapi.com'
-        }
-      };
-
-      try {
-        const response = await axios.request(options);
-        setCommentaryData(response.data);
-        setLoading(false);
-      } catch (error) {
-        setError(error);
-        setLoading(false);
+    const options = {
+      method: 'GET',
+      url: 'https://unofficial-cricbuzz.p.rapidapi.com/matches/get-commentaries',
+      params: { matchId: matchId },
+      headers: {
+        'X-RapidAPI-Key': apikey,
+        'X-RapidAPI-Host': 'unofficial-cricbuzz.p.rapidapi.com'
       }
     };
 
-    fetchCommentaryData();
+    axios.request(options)
+      .then(response => {
+        setCommentaryData(response.data);
+        setLoading(false);
+      })
+      .catch(error => {
+        setError(error);
+        setLoading(false);
+      });
   }, [matchId]);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  if (loading) return <p className='my-4'>Loading...</p>;
+  if (error) return <p className='my-4'>Error fetching data: {error.message}</p>;
 
   return (
     <div className='my-4'>
       <h1>Match Commentary</h1>
-      {commentaryData && (
-        <div>
-          <h2>Match Status: {commentaryData.matchHeaders.status}</h2>
-          <h3>{commentaryData.matchHeaders.matchDesc}</h3>
-          {/* <h3>{commentaryData.matchHeaders.momPlayer.player[0].name}</h3> */}
-          <h4>Commentary:</h4>
-          <ul className='list-group'>
-            {commentaryData.commentaryLines.map((lines,Index) => (
-              <li className='list-group-item' key={Index}>
-               {lines.commentary.commtxt}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <h2>{commentaryData.matchHeaders.status}</h2>
+      <div>
+        {commentaryData.commentaryLines.map((line, index) => {
+          const { commentary } = line;
+          const timestamp = commentary?.timestamp;
+          const commtxt = commentary?.commtxt;
+
+          if (!timestamp || !commtxt) return null;
+
+          return (
+            <div key={index}>
+              <p>{new Date(parseInt(timestamp)).toLocaleString()}</p>
+              <p>{commtxt}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
